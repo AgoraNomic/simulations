a55dbd036db9057f8d6d18e1479f3a5e01d7ce46
Publius Scribonius Scholasticus <p.scribonius.scholasticus@gmail.com>
2017-09-24 17:38:43 -0400

    Fixes

--- sims/economics/no-tax-sim-001.py
+++ sims/economics/no-tax-sim-001.py
@@ -1,23 +1,23 @@
 from random import randint
+from math import floor
 
 
 class Simulation(object):
     def __init__(self, players, total, start):
+        global x
         self.players = players
         for x in players:
-            global x
             x = start
         self.Shinies = total - (players * start)
 
     def iterate(self):
+        global x
         FloatingValue = self.Shinies
         for x in self.players:
-            global x
             Usage = randint(0, 5)
             x = x - Usage * (FloatingValue / 20)
             self.Shinies = self.Shinies - Usage * (FloatingValue / 20)
         for x in floor(self.players / 3):
-            global x
             x = x + 10
             self.Shinies = self.Shinies - 10
 
@@ -26,7 +26,6 @@ class Simulation(object):
             self.iterate
             print(self.Shinies)
             for x in self.players:
-                global x
                 print(x)
 
 
