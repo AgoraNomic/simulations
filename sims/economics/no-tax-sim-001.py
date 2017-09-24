3d47f2c62fe10f0a4934f7197aabcfb29f249b91
Publius Scribonius Scholasticus <p.scribonius.scholasticus@gmail.com>
2017-09-24 17:25:59 -0400

    Basic sim

--- sims/economics/no-tax-sim-001.py
+++ sims/economics/no-tax-sim-001.py
@@ -1,1 +1,23 @@
-import simulations
+from random import randint
+
+
+class Simulation(object):
+    def __init__(self, players, total, start):
+        self.players = players
+        for x in players:
+            x.shinies = start
+        self.Shinies = total - (players * start)
+
+    def iterate(self):
+        FloatingValue = self.Shinies
+        for x in self.players:
+            Usage = randint(0, 5)
+            x.shinies = x.shinies - Usage * (FloatingValue / 20)
+            self.Shinies = self.Shinies - Usage * (FloatingValue / 20)
+
+    def simulate(self, num):
+        for x in num:
+            self.iterate
+            print self.Shinies
+            for x in self.players:
+                print x.shinies
